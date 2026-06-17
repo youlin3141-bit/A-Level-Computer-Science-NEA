7850495222b942daf004ed754e8ad760b466566f
youlin3141-bit <youlin3141@gmail.com>
2026-06-17 09:13:45 +0100

    2

--- Menus.py
+++ Menus.py
@@ -2,8 +2,15 @@ import pygame
 import pygame_gui
 
 def title_page(manager, window):
-    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 275), (200, 50)),  # rectangle shape
+    play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 275), (200, 50)),  # rectangle shape
                                                 text='Play',
                                                 manager=manager)  # button created
-    window.fill(pygame.Color('white'))
+    title = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((300, 100), (200, 50)),
+        text="Title Text",
+        manager=manager
+    )
+    buttons=[title,play_button]
+    window.fill(pygame.Color('blue'))
+    return buttons
 
