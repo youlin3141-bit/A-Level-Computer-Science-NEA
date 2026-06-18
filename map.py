c6e55a820fdabc87ab57233e657eb7b4d272bb28
youlin3141-bit <youlin3141@gmail.com>
2026-06-18 11:55:04 +0100

    initial map function

--- map.py
+++ map.py
@@ -3,6 +3,7 @@ import random
 current_level=0
 map_width=100
 map_height=100
+map_data=[]
 
 class Room:
     def __init__(self,x,y,width,height):
@@ -11,26 +12,35 @@ class Room:
         self.width=width
         self.height=height
     def centre(self):
-        centre_x=(self.x+self.width)/2
-        centre_y=(self.y+self.height)/2
+        centre_x=(self.x+self.width)//2
+        centre_y=(self.y+self.height)//2
         centre_pos=(centre_x,centre_y)
         return centre_pos
-    def generate_room(self,num):
-        global rooms
-        while len(rooms)<num:
-            width=random.randint(5,15)
-            height=random.randint(5,15)
-            x=random.randint(0,map_width-width)
-            y=random.randint(0,map_height-height)
-            new_room = Room(x,y,width,height)
-            if(self.x-1 < new_room.x + new_room.width and
-            self.x + self.width+1 > new_room.x and
-            self.y -1< new_room.y + new_room.height and
-            self.y + self.height+1 > new_room.y):
-                rooms.append(new_room)
-            else:
-                continue
-        return rooms
+    def intersect(self,other):
+        return (
+                self.x - 1 < other.x + other.width and
+                self.x + self.width + 1 > other.x and
+                self.y - 1 < other.y + other.height and
+                self.y + self.height + 1 > other.y
+        )
+def generate_room(num):
+    global rooms
+    attempts=0
+    while len(rooms)<num and attempts<500:
+        width=random.randint(5,15)
+        height=random.randint(5,15)
+        x=random.randint(0,map_width-width)
+        y=random.randint(0,map_height-height)
+        new_room = Room(x,y,width,height)
+        fail=False
+        for room in rooms:
+            if room.intersect(new_room):
+                fail=True
+                break
+        if not fail:
+            rooms.append(new_room)
+        attempts+=1
+    return rooms
 rooms=[]
 starting_room=Room(0,0,5,5)
 rooms.append(starting_room)
